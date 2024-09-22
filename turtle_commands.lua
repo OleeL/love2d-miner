af28f757c89e6cb20f03146f8fe1461bc966f52c
Oli Legg <oliglegg@hotmail.co.uk>
2024-09-22 18:35:16 +0100

    removing solution

--- turtle_commands.lua
+++ turtle_commands.lua
@@ -4,19 +4,7 @@
 -- turtle.turnRight()
 
 local function commands(turtle)
-    local i = 1
-    while (i <= 8) do
-        turtle:forward()
-        i = i + 1
-    end
-    turtle:turnRight()
-    turtle:forward()
-    turtle:turnRight()
-    i = 1
-    while (i <= 8) do
-        turtle:forward()
-        i = i + 1
-    end
+    -- code here
 end
 
 return commands
